3454619ff5327ab846347be905eab491b6803dc2
Chitre <schitre@paypal.com>
2019-03-13 22:49:51 -0700

    adding state to class based component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component{
-    render(){
+    constructor(props){ //particular to JS language
+        //good location to initliaze state as this is called 
+        //when any instance of compoennt is created
+        super(props); //since we are overriding function, makes sense to call predefined steps first
+        //reference to parent constructor function
+
+        this.state = {
+            lat: null //we don't know latitude yet 
+        }; //only place where we do this.state = something..else we always use this.setState
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position), //callback for success
+            (position) => {
+                this.setState({lat: position.coords.latitude});
+            }, //callback for success
             (err) =>  console.log(err)//callback for failure
         );
-        return <div>Latitude:</div>;
+    }
+
+    render(){
+        return <div>Latitude:{this.state.lat}</div>;
     }
 }
 ReactDOM.render(
